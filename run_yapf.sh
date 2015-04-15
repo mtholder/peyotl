986a40d1bb896098e7a5e376ce6359cf77dc23c6
Mark T. Holder <mtholder@gmail.com>
2015-04-15 10:19:32 +0200

    example of yapf script that could be used to format peyotl

---
+++ run_yapf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo setup.py > dev/to-format-list.txt
+for d in dev doc examples extras peyotl scripts standalone_tests tutorials
+do
+    find $d -name "*.py" >> dev/to-format-list.txt
+done
+for f in $(cat dev/to-format-list.txt)
+do
+    echo "formatting $f"
+    yapf -i "$f"
+done
