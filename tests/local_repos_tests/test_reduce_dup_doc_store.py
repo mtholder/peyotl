21217a22ed3ddab59201b966cd357f9baab07b1a
Mark T. Holder <mtholder@gmail.com>
2016-10-17 18:23:45 -0500

    tree coll tests

--- tests/local_repos_tests/test_reduce_dup_doc_store.py
+++ tests/local_repos_tests/test_reduce_dup_doc_store.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-
+# -*- coding: utf-8 -*-
 # put tests here that use local phylesystem
 # see http://opentreeoflife.github.io/peyotl/maintainer/ for setup
 
@@ -10,6 +10,7 @@ from peyotl.test.support.pathmap import get_test_repo_parent
 import os
 
 from peyotl.utility import get_logger
+from peyotl.utility.str_util import slugify
 
 _LOG = get_logger(__name__)
 
@@ -45,6 +46,8 @@ class TestPhylesystem(unittest.TestCase):
         self.assertTrue(p.get_public_url('zz_112').endswith('zz_112.json'))
         a = self.wrapper.taxon_amendments
         self.assertTrue(a.get_public_url('additions-5000000-5000003').endswith('-5000003.json'))
+        c = self.wrapper.tree_collections
+        self.assertTrue(c.get_public_url('TestUserB/fungal-trees').endswith('ngal-trees.json'))
 
     def testStudyIds(self):
         p = self.wrapper.phylesystem
@@ -119,6 +122,89 @@ class TestPhylesystem(unittest.TestCase):
         self.assertEqual(set(), changed)
         # passing a foreign (or nonsense) SHA should raise a ValueError
         self.assertRaises(ValueError, a.get_changed_docs, 'bogus-SHA')
+
+
+    def testSlugify(self):
+        self.assertEqual('simple-test', slugify('Simple Test'))
+        self.assertEqual('no-punctuation-allowed', slugify('No punctuation allowed!?'))
+        self.assertEqual('no-extra-spaces-', slugify('No \t extra   spaces   '))
+        self.assertEqual('untitled', slugify(''))
+        self.assertEqual('untitled', slugify('!?'))
+        # TODO: allow broader Unicode strings and their capitalization rules?
+        # self.assertEqual(u'километр', slugify(u'Километр'))
+        self.assertEqual(u'untitled', slugify(u'Километр'))  # no support for now
+
+
+
+    def testCollectionIndexing(self):
+        c = self.wrapper.tree_collections
+        k = list(c._doc2shard_map.keys())
+        k.sort()
+        expected = ['TestUserB/fungal-trees', 'TestUserB/my-favorite-trees',
+                    'test-user-a/my-favorite-trees', 'test-user-a/trees-about-bees']
+        self.assertEqual(k, expected)
+
+
+    def testCollectionIds(self):
+        c = self.wrapper.tree_collections
+        k = list(c.get_doc_ids())
+        k.sort()
+        expected = ['TestUserB/fungal-trees', 'TestUserB/my-favorite-trees',
+                    'test-user-a/my-favorite-trees', 'test-user-a/trees-about-bees']
+        self.assertEqual(k, expected)
+
+
+    def testCollectionCreation(self):
+        c = self.wrapper.tree_collections
+        # TODO: create a new collection with a unique name, confirm it exists
+
+
+    def testCollectionCopying(self):
+        c = self.wrapper.tree_collections
+        # TODO: copy an existing study under the same user, confirm it's in the right place
+        # TODO: copy an existing study under a new user, confirm it's in the right place
+
+
+    def testNewCollectionIds(self):
+        # We assign each new collection a unique id based on the owner's userid +
+        # the slugified name, serializing with $NAME-2, etc if this id already exists.
+        c = self.wrapper.tree_collections
+        # TODO: fetch an existing study, copy to the other user (id should reflect new username)
+        # TODO: fetch an existing study, save a copy alongside it (should nudge id via serialization)
+        # TODO: create a new study (with the same name) alongside thes (should nudge id via serialization)
+
+
+    def testCollectionDeletion(self):
+        c = self.wrapper.tree_collections
+        # TODO: create a new collection with a unique name, confirm it exists
+        # TODO: delete the collection, make sure it's gone
+
+
+    def testChangedCollections(self):
+        c = self.wrapper.tree_collections
+        c.pull()  # get the full git history
+        # check for known changed collections in this repo
+        changed = c.get_changed_docs('637bb5a35f861d84c115e5e6c11030d1ecec92e0')
+        self.assertEqual(set(), changed)
+        changed = c.get_changed_docs('d17e91ae85e829a4dcc0115d5d33bf0dca179247')
+        self.assertEqual({u'TestUserB/fungal-trees.json'}, changed)
+        changed = c.get_changed_docs('af72fb2cc060936c9afce03495ec0ab662a783f6')
+        expected = {u'test-user-a/my-favorite-trees.json', u'TestUserB/fungal-trees.json'}
+        self.assertEqual(expected, changed)
+        # check a doc that changed
+        changed = c.get_changed_docs('af72fb2cc060936c9afce03495ec0ab662a783f6',
+                                     [u'TestUserB/fungal-trees.json'])
+        self.assertEqual({u'TestUserB/fungal-trees.json'}, changed)
+        # check a doc that didn't change
+        changed = c.get_changed_docs('d17e91ae85e829a4dcc0115d5d33bf0dca179247',
+                                     [u'test-user-a/my-favorite-trees.json'])
+        self.assertEqual(set(), changed)
+        # check a bogus doc id should work, but find nothing
+        changed = c.get_changed_docs('d17e91ae85e829a4dcc0115d5d33bf0dca179247',
+                                     [u'bogus/fake-trees.json'])
+        self.assertEqual(set(), changed)
+        # passing a foreign (or nonsense) SHA should raise a ValueError
+        self.assertRaises(ValueError, c.get_changed_docs, 'bogus')
 
 
 if __name__ == "__main__":
