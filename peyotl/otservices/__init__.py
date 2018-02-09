18a96e39ab051a0dad7890b87a38ee6e0cc206b3
Mark T. Holder <mtholder@gmail.com>
2018-02-09 12:39:59 -0600

    moving to yaml config and less wrapping

--- peyotl/otservices/__init__.py
+++ peyotl/otservices/__init__.py
@@ -70,15 +70,16 @@ def _launch_service(service, cfg):
     logger(__name__).info('Starting {}...'.format(service))
 
 def launch_otcws(cfg):
-    ott_dir = cfg.get_config_setting('ott', 'directory', raise_on_none=True)
+    ott_dir = cfg.get_setting(['ott', 'directory'], raise_on_none=True)
     if not os.path.isdir(ott_dir):
-        raise RuntimeError('[ott]directory "{}" is not a directory'.format(ott_dir))
-    synth_par = cfg.get_config_setting('synthpar', 'directory', raise_on_none=True)
+        raise RuntimeError('ott/directory setting "{}" is not a directory'.format(ott_dir))
+    synth_par = cfg.get_setting('synthpar', 'directory', raise_on_none=True)
     if not os.path.isdir(synth_par):
-        raise RuntimeError('[synthpar]directory "{}" is not a directory'.format(synth_par))
+        raise RuntimeError('synthpar/directory setting "{}" is not a directory'.format(synth_par))
     dir = os.path.join(opentree_config_dir()[0], OTC_TOL_WS)
-    port_num = cfg.get_config_setting('services', 'otcws_port', 1984)
-    num_threads = cfg.get_config_setting('services', 'otcws_num_threads', 1)
+    otc_settings = cfg.get_setting('otcws', raise_on_none=True)
+    port_num = otc_settings.get('port', 1984)
+    num_threads = otc_settings.get('num_threads', 1)
     invoc = ['otc-tol-ws',
              os.path.abspath(ott_dir),
              '--tree-dir={}'.format(os.path.abspath(synth_par)),
--- peyotl/utility.py
+++ peyotl/utility.py
@@ -12,6 +12,7 @@ import logging.config
 import threading
 import logging
 import codecs
+import copy
 import json
 import stat
 import sys
@@ -92,7 +93,6 @@ def configure_logger(cfg_filepath=None,
     else:
         fp, queued = _read_configure_logging_from_fp(filepath=cfg_filepath,
                                                      default_level=default_level)
-    log = logger(__name__)
     log_queued(queued)
     return fp
 
@@ -114,6 +114,7 @@ def _read_configure_logging_from_fp(filepath, default_level=logging.INFO):
         queued.append((logging.DEBUG, m.format(default_level, filepath)))
         filepath = None
     return filepath, queued
+
 
 def log_queued(queued_messages):
     log = logger(__name__)
@@ -443,45 +444,8 @@ def read_as_json(in_filename, encoding='utf-8'):
 
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Functions for interacting with a runtime-configuration.
-Typical usage:
-    setting = get_config_setting('phylesystem', 'parent', default=None)
-Or:
-    cfg = get_config_object()
-    setting = cfg.get_config_setting('phylesystem', 'parent', default=None)
-The latter is more efficient, if you have several settings to check. The former is just sugar for the latter.
+"""For ease of working with hierarchical data, MTH is trying out using a YAML configuration file.
 
-The returned `cfg` from get_config_object is a singleton, immutable ConfigWrapper instance. It is populated based
-on peyotl's system of finding configuration file locations (see below)
-Returned settings are always strings (no type coercion is supported)
-
-
-The ConfigWrapper class also has a
-    setting = cfg.get_from_config_setting_cascade([('apis', 'oti_raw_urls'), ('apis', 'raw_urls')], "FALSE")
-syntax to allow client code to check one section+param name after another
-
-
-It is possible to override settings by creating your own ConfigWrapper instance and using the overrides argument to the
-__init__ call, but that is mainly useful for Mock configurations used in testing.
-
-The function `read_config` is an alias for `get_raw_default_config_and_read_file_list` both should be treated as
-deprecated.
-
-# Peyotl config file finding/ Env variables.
-The default peyotl configuration is read from a file. The configuration filepath is either the value of
-    the PEYOTL_CONFIG_FILE environmental variable (if that variable is defined) or
-    ~/.opentreeoflife/peyotl.ini
-If that filepath does not exist, then the fallback is the config that is the `default_peyotl.ini`
-    file that is packaged with peyotl.
-get_raw_default_config_and_read_file_list() is the accessor for the raw ConfigParser object and list of files
-    that were used to populate that file.
-
-If you want to check your configuration, you can run:
-
-    python scripts/clipeyotl.py config -a list
-
-to see the union of all your settings along with comments describing where those settings come from (e.g. which
-are from your environment).
 
 """
 
@@ -504,9 +468,10 @@ def _replace_default_config(cfg):
 def get_default_config_filename():
     """Returns the configuration filepath.
 
-    If PEYOTL_CONFIG_FILE is in the env that is the preferred choice; otherwise ~/.peyotl/config is preferred.
-    If the preferred file does not exist, then the packaged peyotl/default.conf from the installation of peyotl is
-    used.
+    If PEYOTL_CONFIG_FILE is in the env that is the preferred choice;
+    otherwise ~/.opentreeoflife/peyotl.yaml is preferred.
+    If the preferred file does not exist, then the packaged peyotl/default_peyotl.yaml from
+    the installation of peyotl is used.
     A RuntimeError is raised if that fails.
     """
     global _CONFIG_FN
@@ -518,7 +483,7 @@ def get_default_config_filename():
         env_value = os.environ.get('PEYOTL_CONFIG_FILE')
         cfn = env_value
         if env_value is None:
-            dfn = 'peyotl.ini'
+            dfn = 'peyotl.yaml'
             cfg_par = opentree_config_dir()[0]
             cfn = os.path.join(cfg_par, dfn)
         cfn = os.path.abspath(cfn)
@@ -529,7 +494,7 @@ def get_default_config_filename():
                 logger(__name__).warn(msg.format(cfn, env_value))
             from pkg_resources import Requirement, resource_filename
             pr = Requirement.parse('peyotl')
-            cfn = resource_filename(pr, 'peyotl/default_peyotl.ini')
+            cfn = resource_filename(pr, 'peyotl/default_peyotl.yaml')
         if not os.path.isfile(cfn):
             msg = 'The peyotl configuration file cascade failed looking for "{}"'.format(cfn)
             logger(__name__).warn(msg)
@@ -547,16 +512,22 @@ def get_raw_default_config_and_read_file_list():
     with _CONFIG_LOCK:
         if _CONFIG is not None:
             return _CONFIG, _READ_DEFAULT_FILES
-        try:
-            # noinspection PyCompatibility
-            from ConfigParser import SafeConfigParser
-        except ImportError:
-            # noinspection PyCompatibility,PyUnresolvedReferences
-            from configparser import ConfigParser as SafeConfigParser  # pylint: disable=F0401
-        cfg = SafeConfigParser()
+        import yaml
         cfn = get_default_config_filename()
-        read_files = cfg.read(cfn) if cfn else []
-        _CONFIG, _READ_DEFAULT_FILES = cfg, read_files
+        if cfn:
+            try:
+                instream = codecs.open(cfn, 'r', encoding='utf-8')
+            except:
+                raise RuntimeError('Could not open a configuration file at "{}"'.format(cfn))
+            try:
+                cfg = yaml.load(instream)
+            except Exception as x:
+                m = 'Error parsing configuration file "{}"'.format(cfn)
+                logger(__name__).exception(m)
+                raise RuntimeError('{} parsing exception message: {}'.format(m, str(x)))
+            _CONFIG, _READ_DEFAULT_FILES = cfg, [cfn]
+        else:
+            _CONFIG, _READ_DEFAULT_FILES = {}, []
         return _CONFIG, _READ_DEFAULT_FILES
 
 
@@ -565,9 +536,7 @@ def get_raw_default_config_and_read_file_list():
 read_config = get_raw_default_config_and_read_file_list
 
 
-def _missing_setting_message(section, param, config_filename, warn_on_none_level=logging.WARN):
-    if warn_on_none_level is None:
-        return
+def _missing_setting_message(config_filename, missing_index, setting_list):
     if config_filename:
         if not is_str_type(config_filename):
             f = ' "{}" '.format('", "'.join(config_filename))
@@ -575,40 +544,32 @@ def _missing_setting_message(section, param, config_filename, warn_on_none_level
             f = ' "{}" '.format(config_filename)
     else:
         f = ' '
-    mf = 'Config file {f} does not contain option "{o}"" in section "{s}"'
-    msg = mf.format(f=f, o=param, s=section)
+    if missing_index == 0:
+        mf = '{o}The config file {f} does not contain option "{c}"'
+        o = ''
+        c = '"/"'.join(setting_list)
+    else:
+        mf = 'The "{o}" element of config file {f} does not contain option "{c}"'
+        o = '"/"'.join(setting_list[:missing_index])
+        c = '"/"'.join(setting_list[missing_index:])
+    return mf.format(o=o, f=f, c=c)
 
-def _warn_missing_setting(section, param, config_filename, warn_on_none_level=logging.WARN):
-    msg = _missing_setting_message(section, param, config_filename,
-                                   warn_on_none_level=warn_on_none_level)
-    logger(__name__).warn(msg)
+
+def _warn_missing_setting(config_filename, warn_on_none_level, missing_index, setting_list):
+    if warn_on_none_level is None:
+        return
+    msg = _missing_setting_message(config_filename, missing_index, setting_list)
+    logger(__name__).log(warn_on_none_level, msg)
 
 
-def _raw_config_setting(config_obj,
-                        section,
-                        param,
-                        default=None,
-                        config_filename='',
-                        warn_on_none_level=logging.WARN,
-                        raise_on_none=False):
-    """Read (section, param) from `config_obj`. If not found, return `default`
-
-    If the setting is not found and `default` is None, then an warn-level message is logged.
-    `config_filename` can be None, filepath or list of filepaths - it is only used for logging.
-
-    If warn_on_none_level is None (or lower than the logging level) message for falling through to
-        a `None` default will be suppressed.
-    """
-    # noinspection PyBroadException
+def get_missing_ind_or_value_from_nested_dict(d, key_list):
+    ind = 0
     try:
-        return config_obj.get(section, param)
+        for ind, s in enumerate(key_list):
+            d = d[s]
+        return None, d
     except:
-        if (default is None) and warn_on_none_level is not None:
-            _warn_missing_setting(section, param, config_filename, warn_on_none_level)
-        if raise_on_none:
-            msg = _missing_setting_message(section, param, config_filename)
-            raise RuntimeError(msg)
-        return default
+        return ind, None
 
 
 class ConfigWrapper(object):
@@ -647,24 +608,6 @@ class ConfigWrapper(object):
         self._assure_raw()
         return self._raw
 
-    def get_from_config_setting_cascade(self, sec_param_list, default=None,
-                                        warn_on_none_level=logging.WARN):
-        """return the first non-None setting from a series where each
-        element in `sec_param_list` is a section, param pair suitable for
-        a get_config_setting call.
-
-        Note that non-None values for overrides for this ConfigWrapper instance will cause
-            this call to only evaluate the first element in the cascade.
-        """
-        for section, param in sec_param_list:
-            r = self.get_config_setting(section, param, default=None, warn_on_none_level=None)
-            if r is not None:
-                return r
-        section, param = sec_param_list[-1]
-        if default is None:
-            _warn_missing_setting(section, param, self._config_filename, warn_on_none_level)
-        return default
-
     def _assure_raw(self):
         if self._raw is None:
             cfg, fllist = get_raw_default_config_and_read_file_list()
@@ -672,24 +615,44 @@ class ConfigWrapper(object):
             assert len(fllist) == 1
             self._config_filename = fllist[0]
 
-    def get_config_setting(self,
-                           section,
-                           param,
-                           default=None,
-                           warn_on_none_level=logging.WARN,
-                           raise_on_none=False):
-        if section in self._override:
-            so = self._override[section]
-            if param in so:
-                return so[param]
+    def get_setting(self,
+                    setting_list,
+                    default=None,
+                    warn_on_none_level=logging.WARN,
+                    raise_on_none=False):
         self._assure_raw()
-        return _raw_config_setting(self._raw,
-                                   section,
-                                   param,
-                                   default=default,
-                                   config_filename=self._config_filename,
-                                   warn_on_none_level=warn_on_none_level,
-                                   raise_on_none=raise_on_none)
+        return self._get_from_raw_dict(setting_list,
+                                       default,
+                                       warn_on_none_level,
+                                       raise_on_none)
+
+    def _get_from_raw_dict(self,
+                           key_list,
+                           default,
+                           warn_on_none_level,
+                           raise_on_none):
+        """Read (section, param) from `config_obj`. If not found, return `default`
+
+        If the setting is not found and `default` is None, then an warn-level message is logged.
+        `config_filename` can be None, filepath or list of filepaths - it is only used for logging.
+
+        If warn_on_none_level is None (or lower than the logging level) message for falling through to
+            a `None` default will be suppressed.
+        """
+        # noinspection PyBroadException
+        if self._override:
+            missing_ind, value = get_missing_ind_or_value_from_nested_dict(self._override, key_list)
+            if missing_ind is None:
+                return value
+        missing_ind, value = get_missing_ind_or_value_from_nested_dict(self._raw, key_list)
+        if missing_ind is None:
+            return value
+        if raise_on_none:
+            msg = _missing_setting_message(self._config_filename, missing_ind, key_list)
+            raise RuntimeError(msg)
+        if (default is None) and warn_on_none_level is not None:
+            _warn_missing_setting(self._config_filename, warn_on_none_level, missing_ind, key_list)
+        return default
 
     def report(self, out):
         # noinspection PyProtectedMember
@@ -708,44 +671,36 @@ class ConfigWrapper(object):
             else:
                 emsg = '#  using packaged default. PEYOTL_CONFIG_FILE was not set and ~/.peyotl/config was not found.\n'
         out.write(emsg)
-        from_raw = _create_overrides_from_config(self._raw)
-        k = set(self._override.keys())
-        k.update(from_raw.keys())
-        k = list(k)
-        k.sort()
-        for key in k:
-            ov_set = self._override.get(key, {})
-            fr_set = from_raw.get(key, {})
-            v = set(ov_set.keys())
-            v.update(fr_set.keys())
-            v = list(v)
-            v.sort()
-            out.write('[{s}]\n'.format(s=key))
-            for param in v:
-                if param in ov_set:
-                    out.write('# {p} from override\n{p} = {s}\n'.format(p=param,
-                                                                        s=str(ov_set[param])))
-                else:
-                    out.write('# {p} from {f}\n{p} = {s}\n'.format(p=param,
-                                                                   s=str(fr_set[param]),
-                                                                   f=self._config_filename))
+        from_raw = copy.deepcopy(self._raw)
+        if self._override:
+            merge_into_dict_recursive(from_raw, self._override, out, [], self._config_filename)
 
 
-def _create_overrides_from_config(config):
-    """Creates a two-level dictionary of d[section][option] from a config parser object."""
-    d = {}
-    for s in config.sections():
-        d[s] = {}
-        for opt in config.options(s):
-            d[s][opt] = config.get(s, opt)
-    return d
-
-
-def get_config_setting(section, param, default=None):
-    """
-    """
-    cfg = get_config_object()
-    return cfg.get_config_setting(section, param, default=default)
+def merge_into_dict_recursive(dest, overrides, out, higher, cfg_name):
+    sk = set(dest.keys())
+    kl = list(overrides.keys())
+    kl.sort()
+    for k in kl:
+        dv = dest.get(k)
+        n = higher + [k]
+        if not isinstance(dv, dict):
+            if dv is not None:
+                sk.remove(k)
+            ov = overrides[k]
+            dest[k] = ov
+            out.write('# "{p}" from {s}\n"{p}" = {v}'.format(p='"/"'.join(n),
+                                                             s='override',
+                                                             v=ov))
+        else:
+            merge_into_dict_recursive(dest[k], overrides[k], out, n, cfg_name)
+    sl = list(sk)
+    sl.sort()
+    for k in sl:
+        v = dest[k]
+        n = higher + [k]
+        out.write('# "{p}" from {s}\n"{p}" = {v}'.format(p='"/"'.join(n),
+                                                         s=cfg_name,
+                                                         v=v))
 
 
 def get_config_object():
